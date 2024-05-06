6c94595f2e41c91d41eb48d31202626798403e73
Carl0B <cbarrera18@alumnos.uaq.mx>
2024-05-06 15:43:00 -0600

    Carlo

--- App.jsx
+++ App.jsx
@@ -1,12 +1,16 @@
-import 'react-native-gesture-handler'; // ! NEEDS TO BE HERE AT THE BEGGINING
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { lightColors, darkColors } from './assets/screens/colors/colorsPalettes.jsx';
-import { useColorScheme, View, Text, Image } from 'react-native';
+import { useColorScheme, View, Text, Image, Alert, Linking } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Home, Camera, UrlResults, UrlScan, Academy, CleanExif } from './assets/screens/index.jsx';
+import { Home, UrlResults, UrlScan, Academy, CleanExif } from './assets/screens/index.jsx';
+
+                                      //npm install expo-permissions --unsafe-perm
+                                      //expo install expo-permissions
+import { Camera } from 'expo-camera'; // expo install camare
+import * as MediaLibrary from 'expo-media-library'; //npm install expo-media-library
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -20,21 +24,68 @@ const LogoTitle = () => {
       <Text style={{fontSize: 20, fontWeight: 'bold'}}> secure surf</Text>
     </View>
   );
-};
+}; 
+
+
+const requestPermissionCam = async () => {
+  try {
+      let status = 'denied'; // Inicializamos el estado como denegado para que entre en el ciclo de solicitud
+      //while (status !== 'granted') {
+        const { status: currentStatus } = await Camera.requestCameraPermissionsAsync();
+        status = currentStatus;
+        console.log('Camera permission ',status);
+        //if (status !== 'granted') {
+        //  console.log('Camera permission denied');
+        //  Alert.alert('Permission denied','You need to allow access to use this feature.',
+        //  [{ text: 'OK' },{text: 'Go to Settings',onPress: () => Linking.openSettings(),},]);
+        //} else {
+        //  console.log('Camera permission granted');
+        //} 
+      //}
+    } catch (error) {
+      console.error('Error requesting camera permission:', error);
+    }
+  };
+
+  const requestPermissionFile = async () => {
+    try {
+        let status = 'denied'; // Inicializamos el estado como denegado para que entre en el ciclo de solicitud
+        //while (status !== 'granted') {
+          const { status: currentStatus } = await MediaLibrary.requestPermissionsAsync();
+          status = currentStatus;
+          console.log('File permission ',status);
+          //if (status !== 'granted') {
+          //  console.log('File permission denied');
+          //  Alert.alert('Permission denied','You need to allow access to use this feature.',
+          //  [{ text: 'OK' },{text: 'Go to Settings',onPress: () => Linking.openSettings(),},]);
+          //} else {
+          //  console.log('File permission granted');
+          //}
+        //}
+      } catch (error) {
+        console.error('Error requesting file permission:', error);
+      }
+    };
+
 
 const App = () => {
+  const permissionCam = requestPermissionCam();
+  const permissionFile = requestPermissionFile();
+
   const colorScheme = useColorScheme();
   const palette = colorScheme === 'dark' ? darkColors : lightColors;
 
   return (
-    
+
     <NavigationContainer>
       <Drawer.Navigator 
+
         initialRouteName = "Home" 
         backBehavior='initialRouteName'
         screenOptions={{
           headerTitle: () => <LogoTitle />,
           headerTitleAlign: 'center'
+          
         }}
       >
 
