fa73226fec7a2121044a0861f4e84cbd1043b648
Omsod0404 <garciaomarucho@gmail.com>
2024-05-15 01:49:37 -0600

    SplashScreen added to App

--- App.jsx
+++ App.jsx
@@ -1,82 +1,108 @@
-import 'react-native-gesture-handler'; // ! NEEDS TO BE HERE AT THE BEGGINING DO NOT MOVE OR DELETE!!!!!! 😡
-import React, { useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { lightColors, darkColors } from './assets/screens/colors/colorsPalettes.jsx';
-import { useColorScheme, View, Text, Image, Alert, Linking } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Home, Camera, UrlResults, UrlScan, Academy, CleanExif } from './assets/screens/index.jsx';
-import { useCameraPermissions } from 'expo-camera/next.js';
-import * as MediaLibrary from 'expo-media-library'; //npm install expo-media-library
+import "react-native-gesture-handler"; // ! NEEDS TO BE HERE AT THE BEGGINING DO NOT MOVE OR DELETE!!!!!! 😡
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { lightColors, darkColors } from "./assets/screens/colors/colorsPalettes.jsx";
+import { useColorScheme, View, Text, Image, Alert, Linking, SafeAreaView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Home, Camera, UrlResults, UrlScan, Academy, CleanExif, SplashScreen } from "./assets/screens/index.jsx";
+import { useCameraPermissions } from "expo-camera/next.js";
+import * as MediaLibrary from "expo-media-library"; //npm install expo-media-library
+import { Header } from "./assets/components/index.jsx";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
-const LogoTitle = () => {
-  return (
-    <View style = {{alignContent: 'center', justifyContent: 'center', flexDirection: 'row',}}>
-      <Image 
-        style = {{height: 38, width: 30}}
-        source={require('./assets/images/Slogo.png')}/>
-      <Text style={{fontSize: 20, fontWeight: 'bold'}}> secure surf</Text>
-    </View>
-  );
-}; 
-
 const requestPermissionCam = async () => {
   try {
-      const [permission, requestPermission] = useCameraPermissions();
-      useEffect(() => {( async () => { await requestPermission() } )()},[])
-      console.log('Camera permission ', permission?.status);
-    } catch (error) {
-      console.error('Error requesting camera permission:', error);
-    }
+    const [permission, requestPermission] = useCameraPermissions();
+    useEffect(() => {
+      (async () => {
+        await requestPermission();
+      })();
+    }, []);
+    console.log("Camera permission ", permission?.status);
+  } catch (error) {
+    console.error("Error requesting camera permission:", error);
+  }
 };
 
 const requestPermissionFile = async () => {
   try {
-    const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
-    useEffect(() => {( async () => { await requestPermission() } )()},[])
-    console.log('File permission ', permissionResponse?.status);
+    const [permissionResponse, requestPermission] =
+      MediaLibrary.usePermissions();
+    useEffect(() => {
+      (async () => {
+        await requestPermission();
+      })();
+    }, []);
+    console.log("File permission ", permissionResponse?.status);
   } catch (error) {
-    console.error('Error requesting file permission:', error);
+    console.error("Error requesting file permission:", error);
   }
 };
-
 
 const App = () => {
   const permissionCam = requestPermissionCam();
   const permissionFile = requestPermissionFile();
-
+  const [appIsReady, setAppIsReady] = useState(false);
   const colorScheme = useColorScheme();
-  const palette = colorScheme === 'dark' ? darkColors : lightColors;
+  const palette = colorScheme === "dark" ? darkColors : lightColors;
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
 
   return (
-
-    <NavigationContainer>
-      <Drawer.Navigator 
-
-        initialRouteName = "Home" 
-        backBehavior='initialRouteName'
-        screenOptions={{
-          headerTitle: () => <LogoTitle />,
-          headerTitleAlign: 'center'
-          
-        }}
-      >
-
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="Scan Url" component={UrlScan} />
-        <Drawer.Screen name="Clean Exif" component={CleanExif} />
-        <Drawer.Screen name="Academy" component={Academy} />
-          {/* This screens are not shown in drawer, but must be navigable  */}
-        <Drawer.Screen name="Results" component={UrlResults} options={{ drawerLabel: () => null }}/>
-        <Drawer.Screen name="Camera" component={Camera} options={{ drawerLabel: () => null, headerShown: false }}/>
-
-      </Drawer.Navigator>
-    </NavigationContainer>
-
+    <SafeAreaView style={{ backgroundColor: palette.primary, flex: 1 }}>
+      {appIsReady ? (
+        <>
+          <NavigationContainer>
+            <StatusBar
+              barStyle={
+                colorScheme === "light" ? "dark-content" : "light-content"
+              }
+              backgroundColor={palette.primary}
+            />
+            <Drawer.Navigator
+              initialRouteName="Home"
+              backBehavior="initialRouteName"
+              screenOptions={{
+                headerTitle: () => (
+                  <Header palette={palette} colorScheme={colorScheme} />
+                ),
+                headerStyle: {
+                  backgroundColor: palette.primary,
+                  height: 100,
+                },
+                headerTintColor: palette.secondary,
+                headerTitleAlign: "center",
+              }}
+            >
+              <Drawer.Screen name="Home" component={Home} />
+              <Drawer.Screen name="Scan Url" component={UrlScan} />
+              <Drawer.Screen name="Clean Exif" component={CleanExif} />
+              <Drawer.Screen name="Academy" component={Academy} />
+              {/* This screens are not shown in drawer, but must be navigable  */}
+              <Drawer.Screen name="Results" component={UrlResults} options={{ drawerLabel: () => null }}/>
+              <Drawer.Screen name="Camera" component={Camera} options={{ drawerLabel: () => null, headerShown: false }}/>
+            </Drawer.Navigator>
+          </NavigationContainer>
+        </>
+      ) : (
+        <SplashScreen palette={palette} colorScheme={colorScheme}/>
+      )}
+    </SafeAreaView>
   );
 };
 
--- assets/components/index.jsx
+++ assets/components/index.jsx
@@ -1,6 +1,6 @@
 export {default as Header} from './Header.jsx';
-export {default as Tittle} from './Tittle.jsx';
-export {default as Subtittle} from './Subtittle.jsx';
+export {default as Title} from './Tittle.jsx';
+export {default as Subtitle} from './Subtittle.jsx';
 export {default as ButtonMain} from './ButtonMain.jsx';
 export {default as EXIFContainer} from './EXIFContainer.jsx';
 export {default as Button} from './Button.jsx';
