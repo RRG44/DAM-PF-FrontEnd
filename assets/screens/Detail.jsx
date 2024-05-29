a99e2bf920b632f9738dfa02708bb76a1e839e7b
Omsod0404 <garciaomarucho@gmail.com>
2024-05-29 14:53:27 -0600

    New design on detail and adding the back

--- assets/screens/Detail.jsx
+++ assets/screens/Detail.jsx
@@ -1,54 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View, useColorScheme,ScrollView } from 'react-native';
+import React, {useEffect} from 'react';
+import { StyleSheet, Text, View, useColorScheme,ScrollView, BackHandler } from 'react-native';
 import { lightColors, darkColors } from './colors/colorsPalettes.jsx';
 import { Subtitle, Title} from '../components/index.jsx';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const Detail = ({ route, navigation }) => {
+const Detail = ({ route }) => {
   const { topic } = route.params;
+  const { navigation } = route.params;
   const colorScheme = useColorScheme();
   const palette = colorScheme === 'dark' ? darkColors : lightColors;
 
+  useEffect(() => {
+    const backAction = () => {
+      navigation.navigate('Academy');
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
+    );
+
+    return () => backHandler.remove();
+  }, [navigation]);
+
+
   const styles = StyleSheet.create({
-    container: {
-      display: 'flex',
-      flex: 1,
-      backgroundColor: palette.primary,
-      width: '100%',
-      height:'100%',
-    },
-    title: {
-      fontSize: 24,
-      width: '80%',
-      height:40,
-      alignSelf: 'center',
-    },
-    content: {
-      width: '85%',
-      height:'100%',
-      alignSelf: 'center',
-      flexGrow:1,
-      bottom:10,
-    },
     alltext: {
-      top:15,
       alignSelf: 'center',
       textAlign: 'justify',
       color: palette.secondary,
       lineHeight: 24,
-      marginBottom:30,
+    },
+    backcontainer: {
+      display: 'flex',
+      flex: 1,
+      backgroundColor: palette.primary,
+      width: '100%',
+    },
+    maincontainer: {
+      display: 'flex',
+      flex: 1,
+      backgroundColor: palette.primary,
+      width: '90%',
+      alignSelf: 'center'
     },
   });
 
   return (
-    <View style={styles.container}>
-      <Title palette={palette} style={styles.title}  text="Academy"/>
-      <View style={styles.content}>
-      <Subtitle palette={palette} text={topic.title}/>
-      <ScrollView contentContainerStyle={{ flexGrow: 1}} showsVerticalScrollIndicator={false}>
-      <Text palette={palette} style={styles.alltext}>{topic.content}</Text>
-    </ScrollView>
-    </View>
+    <View style={styles.backcontainer}>
+      <View style={styles.maincontainer}>
+        <Title palette={palette} text="Academy"/>
+        <Subtitle palette={palette} text={topic.title}/>
+        <ScrollView showsVerticalScrollIndicator={false} style={{marginTop:10}}>
+          <Text palette={palette} style={styles.alltext}>{topic.content}</Text>
+        </ScrollView>
+      </View>
     </View>
   );
 };
